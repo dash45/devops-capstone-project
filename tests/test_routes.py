ffd6c45cdfbc18081b09df0f7a69a9d0edd945fd
dash45 <chengyuchan45@gmail.com>
2026-05-25 23:54:37 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -37,7 +37,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-        
+
         talisman.force_https = False
 
     @classmethod
@@ -196,3 +196,11 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+
+            
+    def test_cors_security(self):
+        """应该返回 CORS 头部"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # 检查 CORS 头部
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
